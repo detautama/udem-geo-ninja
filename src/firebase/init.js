2276c98b24fecd5cceeb83c38b25575fef7d12c7
detautama <detautama11@gmail.com>
2018-06-21 18:32:34 +0800

    Install firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDd8hG9VvAU7PM51kVQ0SYr8WipumvP9eE",
+  authDomain: "udemy-geo-ninja-ea6c3.firebaseapp.com",
+  databaseURL: "https://udemy-geo-ninja-ea6c3.firebaseio.com",
+  projectId: "udemy-geo-ninja-ea6c3",
+  storageBucket: "udemy-geo-ninja-ea6c3.appspot.com",
+  messagingSenderId: "577874777320"
+};
+const firebaseApp = firebase.initializeApp(config);
+
+export default firebaseApp.firestore()
